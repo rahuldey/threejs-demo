fe260ac93b09a23823282f02cc8a3ac32e1e9c9d
DIL_User1 <dil_user1@Gavins-MacBook-Pro.local>
2019-05-16 20:56:42 +0800

    saturn

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
-let scene, camera, renderer, torus1, torus2, torus3;
-let ADD = 0.05;
+let scene, camera, renderer, axes;
+let ADD = 0.01;
 
-const createTorus = (x, y, color) => {
-  let geometry = new THREE.TorusGeometry(x, y, 2, 50, 2 * Math.PI);
-  let material = new THREE.MeshBasicMaterial({
-    color: color
-  });
-  let torus = new THREE.Mesh(geometry, material);
-  torus.rotation.x += 1;
-  torus.rotation.y += 0.5;
-  return torus;
+const createAxes = () => {
+  axes = new THREE.AxesHelper(5);
+  scene.add(axes);
 };
 
-const addTorus = () => {
-  torus1 = createTorus(1, 0.225, 0xe9d687);
-  torus2 = createTorus(1.5, 0.225, 0xebc93e);
-  torus3 = createTorus(2, 0.225, 0xe9d687);
-
-  scene.add(torus1);
-  scene.add(torus2);
-  scene.add(torus3);
-};
+const createGeometry = () => {};
 
 const init = () => {
   // create the scene
-
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x000000);
 
   // create and locate the camera
-
   camera = new THREE.PerspectiveCamera(
     30,
     window.innerWidth / window.innerHeight,
@@ -39,13 +23,13 @@ const init = () => {
     1000
   );
   camera.position.z = 10;
+  camera.position.x = 0.5;
+
+  createAxes();
 
   // create the renderer
-
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-
-  addTorus();
 
   document.body.appendChild(renderer.domElement);
 };
---
+++ src/index.saturn.js
@@ -0,0 +1,78 @@
+import * as THREE from 'three';
+
+let scene, camera, renderer, sphere, axes;
+let torus = [];
+let ADD = 0.01;
+
+const createTorus = (x, y, color) => {
+  let geometry = new THREE.TorusGeometry(x, y, 2, 50, 2 * Math.PI);
+  let material = new THREE.MeshBasicMaterial({
+    color: color
+  });
+  let torus = new THREE.Mesh(geometry, material);
+  torus.rotation.x += 2;
+  torus.rotation.y += 0.25;
+  return torus;
+};
+
+const addPlanet = () => {
+  let geometry = new THREE.SphereGeometry(0.7, 30, 30);
+  let material = new THREE.MeshBasicMaterial({
+    color: 0xaf6119
+  });
+  sphere = new THREE.Mesh(geometry, material);
+  scene.add(sphere);
+};
+
+const addRings = () => {
+  torus.push(createTorus(1, 0.225, 0xe9d687));
+  torus.push(createTorus(1.5, 0.225, 0xebc93e));
+  torus.push(createTorus(2, 0.225, 0xe9d687));
+
+  torus.forEach(item => {
+    scene.add(item);
+  });
+};
+
+const createAxes = () => {
+  axes = new THREE.AxesHelper(5);
+  scene.add(axes);
+};
+
+const init = () => {
+  // create the scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000000);
+
+  // create and locate the camera
+  camera = new THREE.PerspectiveCamera(
+    30,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.z = 10;
+
+  // create the renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // populate the scene
+  //createAxes();
+  addRings();
+  addPlanet();
+
+  document.body.appendChild(renderer.domElement);
+};
+
+const mainloop = () => {
+  camera.position.y += ADD;
+
+  if (camera.position.y > 1.75 || camera.position.y < -1.75) ADD *= -1;
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(mainloop);
+};
+
+init();
+mainloop();
